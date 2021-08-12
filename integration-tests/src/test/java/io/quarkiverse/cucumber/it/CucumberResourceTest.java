acb9da9d18adca9ea2d81380c53cee9839405722
Markus Heberling <markus.heberling@advanova.de>
2021-08-12 12:32:47 +0200

    Add support for running with main() method

--- integration-tests/src/test/java/io/quarkiverse/cucumber/it/CucumberResourceTest.java
+++ integration-tests/src/test/java/io/quarkiverse/cucumber/it/CucumberResourceTest.java
@@ -3,5 +3,7 @@ package io.quarkiverse.cucumber.it;
 import io.quarkiverse.cucumber.CucumberQuarkusTest;
 
 public class CucumberResourceTest extends CucumberQuarkusTest {
-
+    public static void main(String[] args) {
+        runMain(CucumberResourceTest.class, args);
+    }
 }
